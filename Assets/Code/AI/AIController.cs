7929f8e917500133e34186056413854d4469c68d
byubyuchan <ehdrjs1410@naver.com>
2025-12-13 18:51:07 +0900

    [Feat] 여러개의 목적지를 랜덤하게 순회하는 군중 구현
    테스트 구체를 캐릭터로 변경하고 이동시 애님이 변경되도록 해야합니다.

--- Assets/Code/AI/AIController.cs
+++ Assets/Code/AI/AIController.cs
@@ -1,14 +1,55 @@
+using System.Linq;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class AIController : MonoBehaviour
 {
-    public GameObject goal;
+    public Transform[] goals;
     NavMeshAgent agent;
+    private int currentGoalIndex = -1;
 
     void Start()
     {
+        FindAllGoalsByTag();
         agent = GetComponent<NavMeshAgent>();
-        agent.SetDestination(goal.transform.position);
+        SetRandomDestination();
+    }
+
+    void Update()
+    {
+        if (agent.hasPath && agent.remainingDistance < agent.stoppingDistance)
+        {
+            SetRandomDestination();
+        }
+    }
+
+    // 무작위 목표 지점을 설정하는 메서드
+    void SetRandomDestination()
+    {
+        if (goals.Length == 0)
+        {
+            Debug.LogError("Goals 배열이 비어 있습니다. 목표 지점을 설정해 주세요.");
+            return;
+        }
+
+        int newGoalIndex;
+
+        do
+        {
+            newGoalIndex = Random.Range(0, goals.Length);
+        } while (newGoalIndex == currentGoalIndex);
+
+        // 새 목표 인덱스 업데이트
+        currentGoalIndex = newGoalIndex;
+
+        // 선택된 목표의 위치로 이동 설정
+        agent.SetDestination(goals[currentGoalIndex].position);
+    }
+
+    void FindAllGoalsByTag()
+    {
+        GameObject[] goalObjects = GameObject.FindGameObjectsWithTag("Goal");
+
+        goals = goalObjects.Select(g => g.transform).ToArray();
     }
 }
