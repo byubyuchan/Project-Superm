b79495324e858c2b22aedaa945b953aefdf49b6e
byubyuchan <ehdrjs1410@naver.com>
2025-12-13 18:24:34 +0900

    [Feat] 군중 시스템 구현
    위치를 향해 AI Navigation을 이용해 길을 찾아 목적지로 이동하는 걸 구현했습니다.
    
    차후 여러 목적지를 두어 목적지에 도착하면, 다른 목적지로 다시 이동하는 로직을 만들면 술래잡기 게임의 군중이 완성됩니다.

---
+++ Assets/Code/AI/AIController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIController : MonoBehaviour
+{
+    public GameObject goal;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(goal.transform.position);
+    }
+}
