70527fb43574f9ee0977328b67a92c7a14ac28dd
byubyuchan <ehdrjs1410@naver.com>
2026-05-28 00:30:46 +0900

    [Feat] 투사체 발사로직 리팩토링 2
    1. 투사체 발사 로직을 수정 중입니다.
    
    2. 레이캐스트로 맞은 오브젝트를 판별 -> 조준선의 방향으로 10000f까지 날아감
    
    3. MoveByKeys에서 최대 사거리를 지정할 수 있습니다. (수명과 더불어 함께 사용 가능)
    
    =========================================FPS 사격 레퍼런스를 조금 찾아봐야겠습니다.
    이도 안된다면 RayCast로 맞은 위치에 투명한 Projectile 오브젝트를 소환하면 히트스캔 판정이 되긴합니다.
    
    현재 이슈는 근거리에서 사격했을 때 논리적이라면 조준선을 향하는 위치로 사격이 돼야하지만, 그저 먼 곳으로만 나가는 로직을 갖고 있습니다.

--- Assets/Code/Player/MoveByKeys.cs
+++ Assets/Code/Player/MoveByKeys.cs
@@ -317,27 +317,10 @@ namespace Photon.Pun.UtilityScripts
             //photonView.RPC("RPC_LoadAction", RpcTarget.All, "ReadyToAttack", isLoadingAttack);
 
             Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-            RaycastHit hit;
+
             Vector3 targetPoint;
 
-            if (Physics.Raycast(ray, out hit, maxRange, ~aimLayerMask))
-            {
-                targetPoint = hit.point;
-
-                // 카메라에서 타겟까지의 거리와, 카메라에서 내 총구까지의 거리를 비교
-                float distToTarget = Vector3.Distance(Camera.main.transform.position, hit.point);
-                float distToMuzzle = Vector3.Distance(Camera.main.transform.position, firePoint.position);
-
-                if (distToTarget < distToMuzzle + 20f)
-                {
-                    targetPoint = firePoint.position + Camera.main.transform.forward * maxRange;
-                }
-            }
-            else
-            {
-                // 아무것도 안 맞았으면 허공의 1000m 앞을 타겟으로! (장거리 버그 완벽 해결)
-                targetPoint = ray.GetPoint(maxRange);
-            }
+            targetPoint = ray.GetPoint(maxRange);
 
             Vector3 aimDirection = (targetPoint - firePoint.position).normalized;
 
